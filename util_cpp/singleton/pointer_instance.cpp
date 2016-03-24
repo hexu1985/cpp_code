41665a2a845173299558a42a0ec8b846f9aa041a
hexu <hexu@a.com>
2016-03-25 00:40:48 +0800

    singleton mt

--- util_cpp/singleton/pointer_instance.cpp
+++ util_cpp/singleton/pointer_instance.cpp
@@ -1,10 +1,13 @@
 #include "singleton.h"
+#include <thread>
+#include <chrono>
 
 static Singleton *instance_;
 
 Singleton &Singleton::GetInstance()
 {
 	if (instance_ == nullptr) {
+		std::this_thread::yield();
 		instance_ = new Singleton;
 	}
 	return *instance_;
--- util_cpp/singleton/singleton.cpp
+++ util_cpp/singleton/singleton.cpp
@@ -6,13 +6,11 @@
 
 Singleton::Singleton()
 {
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	std::cerr << "Singleton::Singleton()\n";
 }
 
 Singleton::~Singleton()
 {
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	std::cerr << "Singleton::~Singleton()\n";
 }
 
