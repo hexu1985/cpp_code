86e42d3ceecc2165b33335131b2975c1e9e75379
hexu <hexu@a.com>
2016-04-01 23:59:47 +0800

    xx

--- util_cpp/macro_add_quotation/test.cpp
+++ util_cpp/macro_add_quotation/test.cpp
@@ -11,6 +11,7 @@ enum class Color {
 };
 
 #define ADD_QUOTATION(x)  #x
+#define	MAKE_PAIR(x)	{x, ADD_QUOTATION(x)}
 
 int main(int argc, char *argv[])
 {
@@ -19,13 +20,18 @@ int main(int argc, char *argv[])
 	};
 
 	typedef std::unordered_map<Color, const char *, decltype(hash_func)> Color2NameMap;
-	Color2NameMap color2name_map({{Color::red, "red"}, {Color::green, "green"}}, 100, hash_func);
+	static const Color2NameMap color2name_map({{Color::red, "red"}, {Color::green, "green"}}, 100, hash_func);
 	for (auto &i : color2name_map) {
 		std::cout << static_cast<int>(i.first) << ": " << i.second << std::endl;
 	}
 
 	Color2NameMap color2name_map2({{Color::red, ADD_QUOTATION(red)}, {Color::green, ADD_QUOTATION(green)}}, 100, hash_func);
 	for (auto &i : color2name_map2) {
+		std::cout << static_cast<int>(i.first) << ": " << i.second << std::endl;
+	}
+
+	Color2NameMap color2name_map3({MAKE_PAIR(Color::red), MAKE_PAIR(Color::green)}, 100, hash_func);
+	for (auto &i : color2name_map3) {
 		std::cout << static_cast<int>(i.first) << ": " << i.second << std::endl;
 	}
 }
